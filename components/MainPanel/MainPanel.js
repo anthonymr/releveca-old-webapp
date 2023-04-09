c44b7c04218c849504bf6dfa572f29082908ee9c
Anthony Martin <anthonymartin20@gmail.com>
2023-04-09 11:51:03 -0400

    Add key to all submodules

--- components/MainPanel/MainPanel.js
+++ components/MainPanel/MainPanel.js
@@ -6,12 +6,12 @@ template: `
             <v_header_location :module="activeModule"></v_header_location>
             <div class="main-panel__padding"></div>
 
-            <div v-if="activeModule?.submodule?.id == 1"><v_items></v_items></div>
-            <div v-else-if="activeModule?.submodule?.id == 3"><v_clients :key="activeModule?.submodule?.id"></v_clients></div>
-            <div v-else-if="activeModule?.submodule?.id == 4"><v_quotes></v_quotes></div>
-            <div v-else-if="activeModule?.submodule?.id == 10"><v_quotes_approval></v_quotes_approval></div>
-            <div v-else-if="activeModule?.submodule?.id == 11"><v_new_client></v_new_client></div>
-            <div v-else-if="activeModule?.submodule?.id == 12"><v_clients :key="activeModule?.submodule?.id" :all="true"></v_clients></div>
+            <div v-if="activeModule?.submodule?.id == 1"><v_items :key="key"></v_items></div>
+            <div v-else-if="activeModule?.submodule?.id == 3"><v_clients :key="key"></v_clients></div>
+            <div v-else-if="activeModule?.submodule?.id == 4"><v_quotes :key="key"></v_quotes></div>
+            <div v-else-if="activeModule?.submodule?.id == 10"><v_quotes_approval :key="key"></v_quotes_approval></div>
+            <div v-else-if="activeModule?.submodule?.id == 11"><v_new_client :key="key"></v_new_client></div>
+            <div v-else-if="activeModule?.submodule?.id == 12"><v_clients :key="key" :all="true"></v_clients></div>
             <div v-else></div>
         </section>
     `,
@@ -20,6 +20,9 @@ template: `
             return Object.keys(this.module).length
                 ? this.module
                 : this.$default;
+        },
+        key() {
+            return this.activeModule?.submodule?.id;
         }
     }
 
